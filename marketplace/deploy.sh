2e16d96a613f63008312b6cb7ae84d192dce4590
Jake Rawsthorne <jake.rawsthorne@couchbase.com>
2021-09-07 11:36:57 +0100

    Don't install couchbase server

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -6,16 +6,9 @@ TEMPLATE_BODY="file://couchbase-byol.template"
 REGION=$2
 
 ServerInstanceCount=$3
-ServerVersion=$4
-ServerUrl=$6
-ServerUsername=$7
-ServerPassword=$8
 ServerDiskSize="100"
-SyncGatewayInstanceCount="0"
 InstanceType="m4.xlarge"
-Username="Administrator"
-Password="password"
-KeyName=$5
+KeyName=$4
 SSHCIDR="0.0.0.0/0"
 
 echo $ServerUrl
@@ -27,14 +20,7 @@ aws cloudformation create-stack \
 --region ${REGION} \
 --parameters \
 ParameterKey=ServerInstanceCount,ParameterValue=${ServerInstanceCount} \
-ParameterKey=ServerVersion,ParameterValue=${ServerVersion} \
-ParameterKey=ServerUrl,ParameterValue=${ServerUrl} \
-ParameterKey=ServerUser,ParameterValue=${ServerUsername} \
-ParameterKey=ServerPassword,ParameterValue=${ServerPassword} \
 ParameterKey=ServerDiskSize,ParameterValue=${ServerDiskSize} \
-ParameterKey=SyncGatewayInstanceCount,ParameterValue=${SyncGatewayInstanceCount} \
 ParameterKey=InstanceType,ParameterValue=${InstanceType} \
-ParameterKey=Username,ParameterValue=${Username} \
-ParameterKey=Password,ParameterValue=${Password} \
 ParameterKey=KeyName,ParameterValue=${KeyName} \
 ParameterKey=SSHCIDR,ParameterValue=${SSHCIDR}
